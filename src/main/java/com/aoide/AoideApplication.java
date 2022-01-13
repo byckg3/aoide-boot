e24501156d53989590b2388ca093af027a4218eb
byc <byckg3@outlook.com>
2022-01-13 15:53:38 +0800

    add new columns

--- src/main/java/com/aoide/AoideApplication.java
+++ src/main/java/com/aoide/AoideApplication.java
@@ -5,12 +5,16 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.context.annotation.Bean;
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
-
+@EnableJpaAuditing
 @SpringBootApplication
 public class AoideApplication
 {
--- src/main/java/com/aoide/user/model/Account.java
+++ src/main/java/com/aoide/user/model/Account.java
@@ -1,7 +1,10 @@
 package com.aoide.user.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
@@ -9,10 +12,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import lombok.Data;
 
 @Data
 @Entity
+@EntityListeners( value = AuditingEntityListener.class )
 @Table( name = "accounts" )
 public class Account implements java.io.Serializable
 {
@@ -33,4 +41,12 @@ public class Account implements java.io.Serializable
 
 	@Enumerated( EnumType.STRING )
 	private Role role;
+	
+	@CreatedDate
+	@Column( name = "created_date", nullable = false, updatable = false )
+	private Date createdDate;
+
+	@LastModifiedDate
+	@Column( name = "modified_date" )
+	private Date modifiedDate;
 }
