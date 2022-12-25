53f90868daa465517a2bb06a33c945deae5503d1
byc <byckg3@gmail.com>
2022-12-26 00:28:26 +0800

    add comment

---
+++ src/main/resources/data.sql
@@ -0,0 +1,28 @@
+-- INSERT INTO accounts( name, role, enabled, created_date, email, password, version )
+-- VALUES ( 'Tester', 'ADMIN', TRUE, '2022-01-01 01:30:00.000', 'test001@email.com', '$2a$10$ARdjIR.M0TQWoS0WiRzY0en06.Td/jdndur.70FrKd6iJM7wPuu7a', 0 );
+
+
+
+-- INSERT INTO grades( class_id, category, student_id, grade )
+-- VALUES ( 'A', '國文', 'A001', 86 );
+
+-- INSERT INTO grades( class_id, category, student_id, grade )
+-- VALUES ( 'A', '數學', 'A001', 97 );
+
+
+-- SELECT category as A班科目, count( grade ) as A級人數80分以上
+-- FROM grades
+-- WHERE grade >= 80 and class_id = 'A'
+-- GROUP BY category
+
+-- SELECT category as A班科目, 
+--        COUNT( CASE WHEN grade BETWEEN 80 AND 100 THEN 1 END )   AS "A級人數80分以上",
+--        COUNT( CASE WHEN grade BETWEEN 60 and 79 THEN 1 END )    AS "B級人數60~79",
+--        COUNT( CASE WHEN grade BETWEEN 0 and 59 THEN 1 END )     AS "C級人數0~59",
+--        AVG( CASE WHEN grade BETWEEN 80 AND 100 THEN grade END ) AS "A級平均分數80分以上",
+--        AVG( CASE WHEN grade BETWEEN 60 and 79 THEN grade END )  AS "B級平均分數60~79",
+--        AVG( CASE WHEN grade BETWEEN 0 and 59 THEN grade END )   AS "C級平均分數0~59"
+-- FROM grades
+-- WHERE class_id = 'A'
+-- GROUP BY category
+-- ORDER BY 2 DESC;
